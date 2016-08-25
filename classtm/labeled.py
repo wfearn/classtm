3e8258bda3ace54eef16ec9e6bb3868cf378e867
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2016-08-25 16:17:11 -0600

    Added code to run the original supervised anchor words algorithm
    i.e., according to Nguyen et al., NAACL 2015

--- classtm/labeled.py
+++ classtm/labeled.py
@@ -25,19 +25,32 @@ def get_labels(filename):
     return labels, classorder
 
 
-class ClassifiedDataset(ankura.pipeline.Dataset):
+def orderclasses(classorder):
+    """Build list of classes, indexed according to classorder"""
+    orderedclasses = [''] * len(classorder)
+    for cla in classorder:
+        orderedclasses[classorder[cla]] = cla
+    return orderedclasses
+
+
+class AbstractClassifiedDataset(ankura.pipeline.Dataset):
+    """For use with classtm models"""
+
+    def __init__(self, dataset, labels, classorder):
+        super(AbstractClassifiedDataset, self).__init__(dataset.docwords,
+                                                        dataset.vocab,
+                                                        dataset.titles)
+        self.labels = labels
+        self.classorder = classorder
+        self.orderedclasses = orderclasses(self.classorder)
+
+
+#pylint:disable-msg=too-few-public-methods
+class ClassifiedDataset(AbstractClassifiedDataset):
     """Classified, as in data is labeled with classes"""
 
     def __init__(self, dataset, labels, classorder):
-        ankura.pipeline.Dataset.__init__(self,
-                                         dataset.docwords,
-                                         dataset.vocab,
-                                         dataset.titles)
-        self.labels = labels
-        self.classorder = classorder
-        self.orderedclasses = [''] * len(self.classorder)
-        for cla in self.classorder:
-            self.orderedclasses[self.classorder[cla]] = cla
+        super(ClassifiedDataset, self).__init__(dataset, labels, classorder)
         # add pseudo labels if necessary
         if not isinstance(dataset, ClassifiedDataset):
             origvocabsize = len(self._vocab)
@@ -50,4 +63,41 @@ class ClassifiedDataset(ankura.pipeline.Dataset):
                 tmp[origvocabsize+self.classorder[label], docnum] = 1
             self._docwords = tmp.tocsc()
         # when compute_cooccurrences gets called, we should get the Q we want
+
+
+class SupervisedAnchorDataset(AbstractClassifiedDataset):
+    """Dataset implementing Nguyen et al. (NAACL 2015)"""
+
+    def __init__(self, dataset, labels, classorder):
+        super(SupervisedAnchorDataset, self).__init__(dataset,
+                                                      labels,
+                                                      classorder)
+        # precompute \bar{Q}
+        ankura.pipeline.Dataset.compute_cooccurrences(self)
+        self._dataset_cooccurrences = self._cooccurrences
+        # fool ankura into calling compute_cooccurrences
+        self._cooccurrences = None
+
+    def compute_cooccurrences(self):
+        orig_height, orig_width = self._dataset_cooccurrences.shape
+        classcount = len(self.classorder)
+        self._cooccurrences = np.zeros((orig_height, orig_width+classcount))
+        self._cooccurrences[:, :-classcount] = self._dataset_cooccurrences
+        # assuming that self._docwords is an instance of a scipy sparse matrix
+        docwords_csr = self._docwords.tocsr()
+        indices = docwords_csr.indices
+        indptr = docwords_csr.indptr
+        data = docwords_csr.data
+        for i in range(orig_height):
+            total = 0
+            for docnum, datum in zip(indices[indptr[i]:indptr[i+1]],
+                                     data[indptr[i]:indptr[i+1]]):
+                if datum > 0:
+                    # count up number of documents with word i
+                    total += 1
+                    # tally up number odcuments with class label
+                    label = self.classorder[self.labels[self.titles[docnum]]]
+                    self._cooccurrences[i, orig_width+label] += 1
+                # normalize tally
+                self._cooccurrences[i, :-classcount] /= total
 
