163b1e26e09ef587e55e035364889b7c0fc8d242
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2017-06-28 15:10:30 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+'''Setup for ActiveTM'''
+
+from distutils.core import setup
+
+setup(
+    name='classtm',
+    version='0.1',
+    description='Supervised topic modeling',
+    license='GPLv3',
+    author='Nozomu Okuda',
+    author_email='Nozomu.Okuda@gmail.com',
+    url='https://github.com/nOkuda/activetm',
+    packages=['classtm']
+)
