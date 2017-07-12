8b4423ba6c7c1260ec88a6513a119738734ca647
Ethan Garofolo <ethan@suchsoftware.com>
2017-07-12 09:57:21 -0600

    Step 2 experiments

---
+++ check/ethan_run_semi_supervised.py
@@ -0,0 +1,46 @@
+"""Check whether smoothing has a good effect or not"""
+import os
+import pickle
+import random
+import subprocess
+import sys
+import time
+
+from activetm import utils
+from classtm import evaluate
+
+
+FILE_DIR = os.path.dirname(__file__)
+REPO_DIR = os.path.join(FILE_DIR, os.pardir)
+OUT_DIR = os.environ.get('CLASSTM_OUT_DIR', '/local/okuda/tmp')
+
+
+def _run_experiments(settingses, num):
+    """Run experiments with settings in settingses, each repeated num times"""
+    for i in range(num):
+        cur_seed = random.randint(0, sys.maxsize)
+        for settings in settingses:
+            print('====', cur_seed, settings, '====', flush=True)
+            start = time.time()
+            subprocess.run([
+                'python3',
+                os.path.join(REPO_DIR, 'incremental_submain.py'),
+                os.path.join(FILE_DIR, settings),
+                OUT_DIR,
+                str(i),
+                str(cur_seed)])
+            print('####', time.time() - start, flush=True)
+
+
+def _run():
+    """Run experiments and plot data"""
+    settingses = [
+        'ethan_semisupervised_free_classifier.settings',
+        'ethan_semisupervised_sank.settings',
+        ]
+    num = 100
+    _run_experiments(settingses, num)
+
+
+if __name__ == '__main__':
+    _run()
